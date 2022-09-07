fd6a10e97c7f8d9465aa953a1187f9c55de23a6b
이승환 <iseunghwan@iseunghwan-ui-MacBookAir.local>
2022-09-07 21:14:39 +0900

    로그인 페이지 UI구현

--- TikiTaka/Pages/Login/Login.js
+++ TikiTaka/Pages/Login/Login.js
@@ -6,80 +6,39 @@
  * @flow strict-local
  */
 import React from 'react';
-import {Text, View, Image, StyleSheet} from 'react-native';
+import {Text, View, Image, StyleSheet, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
 import Logo from '../../assets/images/Logo.png';
 import Insta from '../../assets/images/instaLogo.png';
-const Main = styled.View``;
-
-const MainLogo = styled.View`
-  margin-top: 220px;
-`;
-
-const MainImg = styled.Image`
-  width: 85px;
-  height: 75px;
-  margin: -15px auto;
-`;
-
-const InstaImg = styled.Image`
-  width: 35px;
-  height: 35px;
-`;
-
-const Name = styled.View`
-  flex-direction: row;
-  justify-content: center;
-`;
-
-const LeftText = styled.Text`
-  font-family: 'Anton';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 45px;
-  color: #779874;
-`;
-
-const RightText = styled(LeftText)`
-  color: #ff8f8f;
-`;
-
-const Sign = styled.View`
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid black;
-  margin: 170px auto;
-  padding: 15px 25px;
-  background: rgba(255, 253, 253, 0.8);
-  border-radius: 20px;
-  border: 1.2px solid blue;
-`;
-
-const LoginText = styled.Text`
-  font-family: 'Anton';
-  font-style: normal;
-  font-weight: bold;
-  font-size: 20px;
-  margin-left: 10px;
-  color: #070707;
-  width: 210px;
-`;
+import LinearGradient from 'react-native-linear-gradient';
+import * as S from './style.js';
+import {styles} from './style';
 
 export default function Login() {
   return (
-    <Main>
-      <MainLogo>
-        <MainImg source={Logo} />
-        <Name>
-          <LeftText>TiKi</LeftText>
-          <RightText>TaKa</RightText>
-        </Name>
-      </MainLogo>
-      <Sign>
-        <InstaImg source={Insta}></InstaImg>
-        <LoginText>Sign In With Instagram</LoginText>
-      </Sign>
-    </Main>
+    <S.Main>
+      {/* 중앙 메인 로고 */}
+      <S.MainLogo>
+        <S.MainImg source={Logo} />
+        <S.Name>
+          <S.LeftText>TiKi</S.LeftText>
+          <S.RightText>TaKa</S.RightText>
+        </S.Name>
+      </S.MainLogo>
+
+      <S.Sign>
+        <LinearGradient
+          colors={['#8F8F8F', '#FF8F8F']}
+          start={{x: 0.0, y: 1.0}}
+          end={{x: 1.0, y: 1.0}}
+          style={styles.grediant}>
+          {/* 로그인 버튼 */}
+          <View style={styles.buttonContainer}>
+            <S.InstaImg source={Insta}></S.InstaImg>
+            <S.LoginText>Sign In With Instagram</S.LoginText>
+          </View>
+        </LinearGradient>
+      </S.Sign>
+    </S.Main>
   );
 }
