17fb6d582076bfa5c24685a9053247791d50e629
Youngjin <yjshin229@gmail.com>
2022-09-09 02:27:31 +0900

    fix: ios build error fixed

--- TikiTaka/App.js
+++ TikiTaka/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 
 import {Text, View} from 'react-native';
 import Navbar from './components/navbar/Navbar';
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, DebugInstructions, Header,LearnMoreLinks, ReloadInstructions,} from 'react-native/Libraries/NewAppScreen';
 import styled from 'styled-components/native';
 import Login from './Pages/Login/Login';
 
 const App = () => {
   return (
     <View>
-      <Login />
+      <Navbar />
     </View>
   );
 };
