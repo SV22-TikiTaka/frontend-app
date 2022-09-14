5de112c819c52c56990da100a5719e7764d919c3
Youngjin Shin <yjshin229@gmail.com>
2022-09-14 19:11:44 +0900

    fix: screen showing, css detailed modified

--- TikiTaka/App.js
+++ TikiTaka/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './navigation/Tabs';
-import {View} from 'react-native';
-import Navbar from './components/navbar/Navbar';
-// prettier-ignore
-import styled from 'styled-components/native';
-import History from './Pages/History/History';
 
 export default function App(){
   return (
-    <View>
       <NavigationContainer>
         <Tabs />
       </NavigationContainer>
-    </View>
   );
 };
--- TikiTaka/navigation/Tabs.js
+++ TikiTaka/navigation/Tabs.js
@@ -13,25 +13,22 @@ export default function Tabs(){
     const InboxIcon = '../assets/Icons/Inbox.png';
     const HistoryIcon = '../assets/Icons/History.png';
     const SettingsIcon = '../assets/Icons/Settings.png';
+
     return(
-        <Tab.Navigator tabBarOptions = {{
-            showLabel: false,
-            style: {
-                position: 'absolute',
-                bottom: 25,
-                left: 20,
-                right: 20,
-                elevation: 0,
-                backgroundColor: '#ffffff',
-                borderRadius: 15,
-                height: 90,
-                ...styles.shadow
-            }
-        }}>
+        <Tab.Navigator screenOptions={{
+        headerShown:false,
+        tabBarStyle: {
+            position: 'absolute',
+            paddingBottom: 20,
+            height: 70,
+            borderColor:'#d9d9d9',
+            borderTopWidth:2
+        }
+    }} tabBarOptions= {{ showLabel: false}} >
             <Tab.Screen name = 'Main' component={Main} options = {{
                 tabBarIcon : ({focused}) => (
                     <View style = {{alignItems: 'center', justifyContent:'center', top:10}}>
-                        <Image source={require(MainIcon)}
+                        <Image source={require(MainIcon) }
                         resizeMode = "contain"
                         style = {{
                             width:25,
@@ -40,7 +37,7 @@ export default function Tabs(){
                         }}/>
                         <Text
                             style = {{color: focused ? '#FF8F8F': '#000000', fontSize:12}}>
-                            Main
+                            MAIN
                         </Text>
                     </View>
                 ),
