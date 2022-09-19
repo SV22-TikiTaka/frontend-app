88efaf316d8f21133ff9105e61d5221760a4534c
Leeseunghwan7305 <sun1238799@naver.com>
2022-09-19 16:22:16 +0900

    history 페이지 data 파일 분할

---
+++ TikiTaka/data/historyData.js
@@ -0,0 +1,66 @@
+//Dummy content
+export const DATA = [
+  {
+    isExpanded: false,
+    question: '내 첫인상은 어떤 이미지였어?',
+    type: 'normal',
+    answer: [
+      {id: 1, val: '강아지 상이였어'},
+      {id: 2, val: '귀여웠어'},
+    ],
+  },
+  {
+    isExpanded: false,
+    question: '복권에 당첨된다면?',
+    type: 'normal',
+    answer: [
+      {id: 1, val: '집 살꺼야..'},
+      {id: 2, val: '회사 그만두고 세계여행할꺼야'},
+      {id: 3, val: '기부할래'},
+    ],
+  },
+  {
+    isExpanded: false,
+    question: '오늘 밥 뭐 먹을까?',
+    type: 'normal',
+    answer: [
+      {id: 1, val: '간장게장'},
+      {id: 2, val: '된장찌개'},
+      {id: 3, val: '마라탕'},
+    ],
+  },
+  {
+    isExpanded: false,
+    question: '아무거나 물어봐!',
+    type: 'normal',
+    answer: [
+      {id: 1, val: '마지막으로 연락한 사람은?'},
+      {id: 2, val: '만나는 사람 있어?'},
+      {id: 3, val: '오늘 저녁 뭐 먹을꺼야?'},
+      {id: 4, val: '고양이 이름 뭐야?'},
+      {id: 5, val: '전공이 뭐야?'},
+      {id: 6, val: '가장 좋아하는 계절은?'},
+    ],
+  },
+  //vote
+  //id val count
+  {
+    isExpanded: false,
+    question: '강아지 vs 고양이 vs 토끼',
+    type: 'vote',
+    answer: [
+      {id: 1, val: '강아지', count: 3},
+      {id: 2, val: '고양이', count: 5},
+      {id: 3, val: '토끼', count: 1},
+    ],
+  },
+  {
+    isExpanded: false,
+    question: '엄마 vs 아빠!',
+    type: 'vote',
+    answer: [
+      {id: 1, val: '엄마', count: 7},
+      {id: 2, val: '아빠', count: 10},
+    ],
+  },
+];
