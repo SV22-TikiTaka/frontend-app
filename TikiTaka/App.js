603db617fa4a5b2b0d83a93b1133977cfce06da8
Youngjin Shin <yjshin229@gmail.com>
2022-09-20 11:10:26 +0900

    feat: splashscreen implemented

--- TikiTaka/App.js
+++ TikiTaka/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Tabs from './navigation/Tabs';
 import {QueryClient, QueryClientProvider} from 'react-query';
+import SplashScreen from 'react-native-splash-screen';
+
 const queryClient = new QueryClient();
 export default function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
--- TikiTaka/android/app/src/main/java/com/tikitaka/MainActivity.java
+++ TikiTaka/android/app/src/main/java/com/tikitaka/MainActivity.java
@@ -3,6 +3,8 @@ package com.tikitaka;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +15,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "TikiTaka";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
   }
 
   /**
