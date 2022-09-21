9230b0d0176ee7757b80b49e1a66e12ae4208d53
Youngjin Shin <yjshin229@gmail.com>
2022-09-21 17:38:45 +0900

    design: inbox button deleted

--- TikiTaka/Pages/Inbox/style.js
+++ TikiTaka/Pages/Inbox/style.js
@@ -4,45 +4,20 @@ import { StyleSheet, SafeAreaView } from 'react-native';
 export const InboxContainer = styled.SafeAreaView`
   flex: 1;
   background-color: white;
-
-`
-export const MailOutline = styled.View`
-  width: 25%;
-  height: 95px;
-  margin: 15px auto;
-  border-radius: 25px;
-  background-color: #d9d9d9;
 `;
-
-export const buttonContainer = styled.View`
-  margin-bottom: 70px;
-  align-items: center;
-  justify-content: center;
-`
-export const MoreMessages = styled.TouchableOpacity`
-  border-radius: 20px;
-  background-color: #779874;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  padding: 12px 30px;
-`;
-
 export const StyledText = styled.Text`
   font-size: 15px;
   color: #ffffff;
   font-family: 'SBAggroM';
 `;
 
-export const closedMail = styled.Image`
-  width: 100%;
-  height: 100%;
-`;
-
 export const MailBox = styled.TouchableOpacity`
-  width: 25%;
-  height: 95px;
-  margin: 30px 15px 0px 15px;
+  margin: 15px;
+  background-color: #E9E9E9;
+  padding:15px;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const styles = StyleSheet.create({
@@ -62,12 +37,12 @@ export const styles = StyleSheet.create({
       },
     }),
   },
-
   gridView: {
     flex:3,
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'flex-start',
-    marginLeft: 15,
+    marginVertical: 20,
+    marginHorizontal:15
   },
 });
