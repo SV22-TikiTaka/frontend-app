7a115648da305c3a41d6184dc39bf3bf76ad978d
Youngjin Shin <yjshin229@gmail.com>
2022-09-26 18:53:56 +0900

    history page api modified
    wrapped with useEffect()

--- TikiTaka/Pages/History/History.js
+++ TikiTaka/Pages/History/History.js
@@ -1,68 +1,12 @@
 import React, {useState, useEffect,useContext} from 'react';
 //prettier-ignore
-import {View,Text,ScrollView,TouchableOpacity,StyleSheet,SafeAreaView,Button,LayoutAnimation,Platform,UIManager,} from 'react-native';
+import {View,ScrollView,SafeAreaView,LayoutAnimation,Platform,UIManager} from 'react-native';
 import Header from '../../components/Header/Header';
 import * as S from './style.js';
 import {styles} from './style';
 import {DATA} from './data';
 import themeContext from '../../config/themeContext.js';
 import axios from 'axios';
-
-
-const datalist = new Array();
-
-//make datalist and push questions
-axios({
-  url: 'http://localhost:8000/api/v1/questions/history/1',
-  method: "get"
-}).then((response) => {
-  const questions = response.data;
-  for(let i = 0; i< questions.length-1;i++){
-    const data = new Object();
-    data.isExpanded = false;
-    data.question_id = questions.id;
-    data.question = questions.content;
-    data.type = questions.type;
-    data.commentType = questions.comment_type;
-    data.answer = [];
-
-    datalist.push(data);
-  }
-})
-
-//iterate through data and push comments in answer
-for(let i = 0; i < datalist.length -1; i++){
-  if (datalist[i][type] === 'normal' || datalist[i][type] === 'challenge'){
-      axios({
-      url: 'http://localhost:8000/api/v1/questions/' + datalist[i]['question_id'],
-      method: "get",
-  }).then((response) => {
-    const q_comments = response.data;
-    const data = new Object();
-    data.id = q_comments.id;
-    data.val = q_comments.content; //음성파일이면 S3경로가 여기, 텍스트면 일반 string
-    datalist[i][answer].push(data);
-  })
-  }else{
-    axios({
-      url: 'http://localhost:8000/api/v1/vote/' + datalist[i]['question_id'],
-      method: "get",
-    }).then((response) => {
-      const v_options = response.data.options;
-      const v_count = response.data.count;
-      for(let j = 0; j < v_options.length -1; j++){
-        const data = new Object();
-        data.id = j;
-        data.val = v_options[j];
-        data.count = v_count[j];
-
-        datalist[i][answer].push(data);
-      }
-    })
-  }
-  
-  }
- 
 
 const ExpandableComponent = ({item, onClickFuntion}) => {
   const [layoutHeight, setlayoutHeight] = useState(0);
@@ -149,11 +93,68 @@ const History = () => {
     setData(array);
   };
 
+  const [Datalist, setDatalist] = useState([]);
+
+  //make datalist and push questions
+  useEffect(() =>{
+    axios.get('http://0.0.0.0:8000/api/v1/questions/history/1')
+    .then(response => {
+      const questions = response.data;
+      const datalist = [];
+
+      for(let i = 0; i< questions.length-1;i++){
+        const data = new Object();
+        data.isExpanded = false;
+        data.question_id = questions.id;
+        data.question = questions.content;
+        data.type = questions.type;
+        data.commentType = questions.comment_type;
+        data.answer = [];
+
+        datalist.push(data);
+      }
+      setDatalist(datalist);
+    })
+  });
+
+  //iterate through data and push comments in answer
+  for(let i = 0; i < Datalist.length -1; i++){
+    if (Datalist[i][type] === 'normal' || Datalist[i][type] === 'challenge'){
+        axios({
+        url: 'http://0.0.0.0:8000/api/v1/questions/' + Datalist[i]['question_id'],
+        method: "get",
+    }).then((response) => {
+      const q_comments = response.data;
+      const data = new Object();
+
+      data.id = q_comments.id;
+      data.val = q_comments.content; //음성파일이면 S3경로가 여기, 텍스트면 일반 string
+      Datalist[i][answer].push(data);
+    })
+    }else{
+      axios({
+        url: 'http://0.0.0.0:8000/api/v1/vote/' + Datalist[i]['question_id'],
+        method: "get",
+      }).then((response) => {
+        const v_options = response.data.options;
+        const v_count = response.data.count;
+        for(let j = 0; j < v_options.length -1; j++){
+          const data = new Object();
+          data.id = j;
+          data.val = v_options[j];
+          data.count = v_count[j];
+
+          Datalist[i][answer].push(data);
+        }
+      })
+    }
+    }
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: theme.background}}>
       <Header Title={Title} TitleColor={TitleColor} />
       <ScrollView>
-        {data.map((item, key) => (
+        {Datalist.map((item, key) => (
           <ExpandableComponent
             key={item.question}
             item={item}
