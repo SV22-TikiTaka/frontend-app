ec558ae6526ba01a9dc2f439d4aa54fcb1e7c6cb
Youngjin Shin <yjshin229@gmail.com>
2022-09-29 21:42:02 +0900

    feat: instagram login with webview

--- TikiTaka/Pages/Login/Login.js
+++ TikiTaka/Pages/Login/Login.js
@@ -5,24 +5,54 @@
  * @format
  * @flow strict-local
  */
-import React, {useContext} from 'react';
+import React, {useContext, Component} from 'react';
 import {Text, View, Image, StyleSheet, TouchableOpacity} from 'react-native';
 import logoWname from '../../assets/images/logoWname.png';
 import Insta from '../../assets/images/instaLogo.png';
 import * as S from './style.js';
 import {styles} from './style';
-import themeContext from '../../config/themeContext.js';
+import InstagramLogin from 'react-native-instagram-login';
 
-const Login = ({navigation}) =>{
-  const theme = useContext(themeContext);
-  return (
-    <S.Main style ={{backgroundColor : theme.background}}>
+
+export default class Login extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: '',
+    };
+  }
+
+  setIgToken = (data) => {
+    console.log('data', data)
+    this.setState({ token: data.access_token })
+  }
+  render () {
+    return(
+        <S.Main style ={{backgroundColor : 'white'}}>
         <S.MainImg source={logoWname} style = {{resizeMode:'contain'}}/>
-          <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate('Home')}>
-              <S.InstaImg source={Insta}></S.InstaImg>
-              <S.LoginText style ={{color : theme.color}}>Sign in with Instagram</S.LoginText>
-           </TouchableOpacity>
-    </S.Main>
-  );
+        {/* onpress = { () =>  navigation.navigate('Home') } */}
+        <TouchableOpacity style={styles.buttonContainer} onPress={() => this.instagramLogin.show()}>
+            <S.InstaImg source={Insta}></S.InstaImg>
+            <S.LoginText style ={{color : 'black'}}>Sign in with Instagram</S.LoginText>
+          </TouchableOpacity>
+          <Text style = {{margin: 10, color: 'black'}}>Token :{this.state.token} </Text>
+          {this.state.failure && (
+          <View>
+            <Text style={{ margin: 10 }}>
+              failure: {JSON.stringify(this.state.failure)}
+            </Text>
+          </View>
+        )}
+          <InstagramLogin 
+            ref = {ref => (this.instagramLogin = ref)}
+            appId='615031730032307'
+            appSecret='97bdb68956f979d4bb21ba82a51e2221'
+            redirectUrl = 'https://github.com/'
+            scopes = {['user_profile', 'user_media']}
+            onLoginSuccess = {this.setIgToken}
+            onLoginFailure={(data) => console.log(data)}
+          />
+    </S.Main> 
+    )
+  }
 }
-export default Login;
